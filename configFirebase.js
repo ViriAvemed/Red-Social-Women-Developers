3a4682ae8702f06d89c75da71150fcf769052152
Avellaneda <vir>
2019-08-18 08:05:05 -0500

    Subiendo estilo css del muro

--- configFirebase.js
+++ configFirebase.js
@@ -8,19 +8,3 @@ const firebaseConfig = {
     appId: "1:792801296519:web:258f0469db05285f"
 };
 firebase.initializeApp(firebaseConfig);
-
-// const db = firebase.firestore();
-//
-// db.collection("users").add({
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-// })
-//     .then(function(docRef) {
-//         console.log("Document written with ID: ", docRef.id);
-//     })
-//     .catch(function(error) {
-//         console.error("Error adding document: ", error);
-//     });
-//
-// console.log("conectado");
