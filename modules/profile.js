1d09b0e10d9a18bfda7e8e6492b53135a18d34cc
Avellaneda <vir>
2019-08-19 12:05:01 -0500

    css perfil

--- modules/profile.js
+++ modules/profile.js
@@ -1,7 +1,38 @@
 let Profile = {
     render : async () => {
         let view = `
-            <h1>ESTE ES MI PERFIL</h1>
+           
+             <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <h1 class="misGrupos">Cristina Suárez</h1>
+                </div>
+             </div>
+             <div class="row">
+                 <div class="col d-flex justify-content-center  ">
+                    <img class="fotoPerfil" src="img/persona2.png" alt="">
+                 </div>
+            </div>
+            
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    </p><i class="icon-pencil"></i>
+                </div>
+             </div>
+             <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <p class=""> Vivo en CDMX, México</p>
+                </div>
+             </div>
+             <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <p class="">Tengo 30 años.</p>
+                </div>
+             </div>
+             <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <p class="">Soy estudiante en Laboratoria</p>
+                </div>
+             </div>
         `;
         return view
 
